0d9be28faeb3ac157c6f82a70a8474c944535c2a
Anthony Giuliano <agiuliano@gmail.com>
2023-09-30 13:52:45 -0700

    Spacing

--- scripts/shell/sfdmu.sh
+++ scripts/shell/sfdmu.sh
@@ -1,7 +1,7 @@
 #! bin/zsh
 # https://help.sfdmu.com/key-features
 
-SCRIPT_DIR="$( cd "$(dirname "$0")" || exit ; pwd -P )"
+SCRIPT_DIR="$(cd "$(dirname "$0")" || exit ; pwd -P)"
 SFDMU_DIR=$SCRIPT_DIR/sfdmu
 mkdir -p $SFDMU_DIR
 sf sfdmu run --path ${SFDMU_DIR} --simulation --sourceusername ag.dev --targetusername ag.integration --usesf
