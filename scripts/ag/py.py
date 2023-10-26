4ca4fab3d788d6496cfd2bc454f62985832bf7cf
Anthony Giuliano <agiuliano@gmail.com>
2023-10-25 18:50:32 -0700

    Py

--- scripts/ag/py.py
+++ scripts/ag/py.py
@@ -1,126 +1,249 @@
 #! /usr/bin/env python3
 
-from typing import Any # https://docs.python.org/3/library/typing.html
-import os
-from os.path import join, dirname # https://docs.python.org/3/library/os.html
-from dotenv import load_dotenv # https://pypi.org/project/python-dotenv/
-import subprocess # https://docs.python.org/3/library/subprocess.html
-import re
-import json # https://docs.python.org/3/library/json.html
-import xml.etree.ElementTree as ET # https://docs.python.org/3/library/xml.etree.elementtree.html
-import pandas as pd # https://pandas.pydata.org/docs/user_guide/index.html#user-guide
-from simple_salesforce import Salesforce # https://simple-salesforce.readthedocs.io/en/latest/
+from typing import Any  # https://docs.python.org/3/library/typing.html
+import os  # https://docs.python.org/3/library/os.html
+import subprocess  # https://docs.python.org/3/library/subprocess.html
+import re  # https://docs.python.org/3/library/re.html
+import json  # https://docs.python.org/3/library/json.html
+import xml.etree.ElementTree as ET  # https://docs.python.org/3/library/xml.etree.elementtree.html
+from lxml import etree  # https://lxml.de/tutorial.html
+import pandas as pd  # https://pandas.pydata.org/docs/user_guide/index.html#user-guide
 
-###### <Psuedocode>
-# Get target environment variables
-	# Find nodes with values that look like email addresses or usernames
-	# Get the files containing those nodes from the target org
-	# Inspect the corresponding nodes in the metadata from the target org
-	# If the first part of the username matches the first part of the node value, then replace the source node value with the target node value
-	# Else replace the source node value based on user mapping
-		# Either
-			# Get target users
-			# Split target users on @
-			# Split element[1] on .
-			# Create list of dicts
-				# TargetUsers = [username: {name, domain, tld, suffix}]
-			# Search force-app/main/default/*.xml nodes for targetUsers
-			# Potentially filter for nodes 'assignedTo', 'notifyTo'
-			# Replace node values with corresponding targetUsers
-		# Or
-			# Get specified map of source users to target users from environment variables
-# Save files
-# Commit changes
-# Proceed with workflow
 
-###### </Psuedocode>
+def sf_cli_to_dict(command: str) -> dict:
+    os.environ["SF_CONTENT_TYPE"] = "JSON"
+    pattern_stdout: re.Pattern[str] = re.compile(pattern=patterns["ANSI"])
+    shell: bool = False
+    args: list[str] | str = command
 
-# Get access token from sf-cli
-sf_stdout: subprocess.CompletedProcess[bytes] = subprocess.run(args=['sf','org','display','user','--json'], stdout=subprocess.PIPE)
+    # sf data query commands should run in a shell and not be split into a list
+    if command.startswith("sf data query"):
+        shell: bool = True
+    else:
+        args = command.split(sep=" ")
 
-# Regex pattern to cleanup output
-## '\x1b\[.*?m' matches ANSI escape sequences
-## '\n' matches newlines
-pattern_stdout: re.Pattern[str] = re.compile(pattern=r'\x1b\[.*?m|\n')
+    output: subprocess.CompletedProcess[bytes] = subprocess.run(
+        args=args, shell=shell, stdout=subprocess.PIPE
+    )
 
-sf_json: str = pattern_stdout.sub(repl='', string=sf_stdout.stdout.decode())
-sf_dict: dict = json.loads(s=sf_json)['result']
+    # Dict from output of sf-cli command
+    output_json: str = pattern_stdout.sub(repl="", string=output.stdout.decode())
+    output_dict: Any = json.loads(s=output_json)
+    return output_dict
+
+
+def process_failures(failure: pd.Series) -> pd.Series:
+    xml_path: str = failure["filePath"]
+    error: str = failure["error"]
+    find_pattern: re.Pattern[str] | None = None
+    fix_function: function | None = None
+
+    for pattern, obj in error_patterns.items():
+        if pattern.search(string=error):
+            find_pattern = obj["FIND"]
+            fix_function = obj["ACTION"]
+            continue
+
+    parse_xml(xml_path=xml_path, find_pattern=find_pattern, fix_function=fix_error)
+    return failure
+
+
+def parse_xml(
+    xml_path: str, find_pattern: re.Pattern[str] | None, fix_function: function | None
+) -> None:
+    tree: Any = etree.parse(source=xml_path, parser=None)
+    for elem in tree.iter():
+        elem_text: str = elem.text if elem.text else ""
+
+        # Check if the element's text looks like an email address
+        # match: re.Match[str] | None = error_pattern.search(string=elem_text)
+        match = False
+
+        if match:
+            match_name: str = match.group("match_name")
+            match_suffix: str = match.group("match_suffix")
+
+            print("--- match ---")
+            print(f"match_name: {match_name}")
+            print(f"match_suffix: {match_suffix}")
+
+            user: pd.DataFrame = users_df.loc[
+                (users_df["Username_Name"] == match_name)
+                & (users_df["Username_Suffix"] == match_suffix),
+                :,
+            ]
+
+            username_new: str = users_df.loc[
+                (users_df["Username_Name"] == match_name),
+                "Username",
+            ].values[0]
+            print(f"username_new: {username_new}")
+
+            email_new: str = users_df.loc[
+                (users_df["Username_Name"] == match_name),
+                "Email",
+            ].values[0]
+            print(f"email_new: {email_new}")
+
+            print("--- elem ---")
+            print(f"elem.attrib: {elem.attrib}")
+            print(f"elem.tag: {elem.tag}")
+            print(f"elem.text: {elem.text}")
+
+
+def replace_invalid_users():
+    pass
+
+
+def replace_invalid_urls():
+    pass
+
+
+def add_missing_layout_fields():
+    pass
+
+
+# Top level domains to look for in usernames
+tlds: list[str] = [
+    "com",
+    "org",
+    "net",
+    "edu",
+    "gov",
+    "biz",
+    "info",
+]
+
+# Suffixes to look for in usernames
+envs: list[str] = [
+    "dev",
+    "qa",
+    "uat",
+    "prod",
+]
+
+# Relevant paths for this script
+paths: dict[str, str] = {
+    "script": os.path.dirname(p=os.path.abspath(path=__file__)),
+    "metadata": os.path.abspath(
+        path=os.path.join(
+            os.path.dirname(p=os.path.abspath(path=__file__)), "..", "..", "force-app"
+        )
+    ),
+    "tmp": os.path.abspath(
+        path=os.path.join(
+            os.path.dirname(p=os.path.abspath(path=__file__)), "..", "..", "tmp"
+        )
+    ),
+}
+
+error_patterns: dict[re.Pattern[str], dict[str, Any]] = {
+    re.compile(
+        pattern=r"no User named \b(?P<match_name>[A-Za-z0-9._%+-]+)(?P<match_domain>@[^@])+\.(?P<match_suffix>[A-Za-z]+)\b found"
+    ): {
+        "FIND": re.compile(
+            pattern=r"\b(?P<match_name>[A-Za-z0-9._%+-]+)@[^@]+\.(?P<match_suffix>[A-Za-z]+)\b"
+        ),
+        "ACTION": replace_invalid_users,
+    },
+    re.compile(
+        pattern=r"\bThe default certificate cannot be selected as the Request Signing Certificate\b"
+    ): {
+        "FIND": re.compile(
+            pattern=r"https://(www\.)?(?P<instance>[^/]+)\.my\.salesforce\.com"
+        ),
+        "ACTION": replace_invalid_urls,  # TODO: replace with instance url from org
+    },
+    re.compile(
+        pattern=r"\bLayout must contain an item for required layout field: (?P<match_field>[A-Za-z0-9._%+-])+"
+    ): {
+        "FIND": None,  # TODO: find end of page layout xml
+        "ACTION": add_missing_layout_fields,
+    },
+}
+
+# Regex patterns
+patterns: dict[str, re.Pattern[str]] = {
+    "ANSI": re.compile(pattern=r"\x1b\[.*?m|\n"),
+    "EMAIL": re.compile(
+        pattern=r"\b(?P<match_name>[A-Za-z0-9._%+-]+)@[^@]+\.(?P<match_suffix>[A-Za-z]+)\b"
+    ),
+    "INSTANCE_URL": re.compile(
+        pattern=r"https://(www\.)?(?P<instance>[^/]+)\.my\.salesforce\.com"
+    ),
+    "REQUIRED_LAYOUT_FIELD": re.compile(
+        pattern=r"\b(?P<match_field>[A-Za-z0-9._%+-])+\b"
+    ),
+}
+
+# Deployment errors and their corresponding patterns
+errors: dict[re.Pattern[str], re.Pattern[str]] = {
+    re.compile(pattern=r"no User named [^@]+@[^@]+\.[^@]+ found"): patterns["EMAIL"],
+    re.compile(
+        pattern=r"\bThe default certificate cannot be selected as the Request Signing Certificate\b"
+    ): patterns["INSTANCE_URL"],
+    re.compile(
+        pattern=r"\bLayout must contain an item for required layout field: (?P<match_field>[A-Za-z0-9._%+-])+"
+    ): patterns["REQUIRED_LAYOUT_FIELD"],
+}
+
+sf_deploy_result: dict = sf_cli_to_dict(
+    command=f"sf project deploy start --source-dir {paths['metadata']} --test-level RunLocalTests --dry-run --concise"
+)["result"]
+
+sf_deploy_failures: dict = sf_deploy_result["files"]
+failures_df: pd.DataFrame = pd.DataFrame.from_dict(
+    data=sf_deploy_failures, orient="columns"
+)
+
+sf_query_users_result: dict = sf_cli_to_dict(
+    command="sf data query --query 'SELECT Id, FirstName, LastName, Email, Username FROM User'"
+)["result"]
+users_df: pd.DataFrame = pd.DataFrame.from_dict(
+    data=sf_query_users_result["records"], orient="columns"
+)
 
 # Authenticate to target org using access token
-sf: Salesforce = Salesforce(instance_url=sf_dict['instanceUrl'],session_id=sf_dict['accessToken'])
+# sf: Salesforce = Salesforce(
+#     instance_url=sf_org_result["instanceUrl"],
+#     session_id=sf_org_result["accessToken"],
+# )
 
-# Get users from target org
-users = sf.query(query='SELECT Id, FirstName, LastName, Email, Username FROM User')['records']
-
-# Drop 'attributes' key from each user dict
-[user.pop('attributes', None) for user in users]
+# # Get users from target org
+# users = sf.query(query="SELECT Id, FirstName, LastName, Email, Username FROM User")[
+#     "records"
+# ]
 
 # Create pd.DataFrame from users
-users_df: pd.DataFrame = pd.DataFrame(data=users)
+# users_df: pd.DataFrame = pd.DataFrame(data=users)
 
 # Split username on @ and expand into new columns
-users_df[['Username_Name', 'Username_Domain']] = users_df['Username'].str.split(pat='@', expand=True)
+users_df[["Username_Name", "Username_Domain"]] = users_df["Username"].str.split(
+    pat="@", expand=True
+)
 
-# Split the Username_Domain column on '.' and expand into new columns with dynamic names
-split_cols: pd.DataFrame = users_df['Username_Domain'].str.split(pat='.', expand=True)
-split_cols.columns = [f'Username_Domain_{i}' for i in range(len(split_cols.columns))]
-users_df = pd.concat([users_df, split_cols], axis=1)
+# Create new column for username tld, i.e. com, org, etc.
+# Uses the intersection of the tlds list and the result of splitting on . in order to handle
+# cases where usernames have a suffix like .prod, .uat, etc.
+users_df["Username_Tld"] = (
+    users_df["Username_Domain"]
+    .str.split(pat=".")
+    .apply(func=lambda strs: next(iter(set(strs) & set(tlds)), None))
+)
 
-# Get the absolute path to the directory containing the script
-script_dir: str = os.path.dirname(p=os.path.abspath(path=__file__))
+# Create new column for username suffix, i.e. everything between @ and .com, .org, etc.
+users_df["Username_Suffix"] = users_df.apply(
+    func=lambda row: row["Username_Domain"].split(sep=f".{row['Username_Tld']}")[0],
+    axis=1,
+)
 
-# Construct the absolute path to the force-app directory
-metadata_dir: str = os.path.abspath(path=os.path.join(script_dir, '..', '..', 'force-app'))
+failures_df.apply(func=process_failures, axis=1)
 
+pass
 # Iterate recursively over the directory tree and find all XML files
-xml_paths: list[Any] = []
-for root_path, dirs, files in os.walk(top=metadata_dir):
-	for file in files:
-		if file.endswith('.xml'):
-			xml_paths.append(os.path.join(root_path, file))
-
-# Regex pattern to match email addresses in xml nodes
-pattern_email: re.Pattern[str] = re.compile(pattern=r'(?P<username_name>[^@]+)@[^@]+\.[^@]+')
-
-# Usernames from target org
-usernames: list[str] = users_df['Username'].tolist()
-
-# Emails from target org
-emails: list[str] = users_df['Email'].tolist()
-
-# Process each XML file
-for xml_path in xml_paths:
-	# Parse the XML file
-	tree: ET.ElementTree = ET.parse(source=xml_path)
-	root: ET.Element = tree.getroot()
-
-	# Recursively process each element in the XML file
-	for elem in root.iter():
-		elem_text: str = elem.text if elem.text else ''
-		# Check if the element's text looks like an email address
-		match: re.Match[str] | None = re.match(pattern=pattern_email, string=elem_text)
-		if match:
-			username_name: str = match.group('username_name')
-			username_new: str = users_df.loc[users_df['Username_Name'] == username_name, 'Username'].values[0]
-			email_new: str = users_df.loc[users_df['Username_Name'] == username_name, 'Email'].values[0]
-			xml_basename: str = os.path.basename(p=xml_path)
-			print('------------------')
-			print(f'username_name: {username_name}')
-			print(f'username_new: {username_new}')
-			print(f'email_new: {email_new}')
-			print(f'user_current: {elem.text}')
-			print('---------')
-			print(f'xml_basename: {xml_basename}')
-			print(f'root.tag: {root.tag}')
-			print(f'elem.attrib: {elem.attrib}')
-			print(f'elem.tag: {elem.tag}')
-
-			# Determine if the value is an email address or a username
-			#! TODO
-			#? If value.split(sep='.')[:-1] in [username.split(sep='.')[:-1] for username in usernames]?
-			#? Else replace with email?
-			# Replace the username with the new username
-			# elem.text = elem_text.replace(username_name, username_new)
-
-			# Write the updated XML file to disk
-			# tree.write(file_or_filename=xml_path)
+# xml_paths: list[str] = []
+# for root_path, dirs, files in os.walk(top=paths["metadata"]):
+#     if paths["metadata"] in root_path:
+#         for file in files:
+#             if file.endswith(".xml"):
+#                 xml_paths.append(path.join(root_path, file))
